fd109390841c12bfe3bc2fc6cd7ac0676d1570c8
paovang <vangpaovang@gmail.com>
2023-12-19 15:01:04 +0700

    update

--- src/modules/user/data-typeorm/services/write.service.ts
+++ src/modules/user/data-typeorm/services/write.service.ts
@@ -5,7 +5,7 @@ import { RoleModel } from './../models/role.model';
 import { hash } from 'bcrypt';
 import { UserModel } from './../models/user.model';
 import { DatabaseConnection } from '@/common/configurations/typeorm.config';
-import { DataSource } from 'typeorm';
+import { DataSource, In } from 'typeorm';
 import { UserEntity } from '@/modules/user/domain/entities/user.entity';
 import { IWriteUserRepository } from '../../domain/repositories/user.interface';
 import { Injectable, InternalServerErrorException, Provider } from '@nestjs/common';
@@ -33,14 +33,9 @@ export class WriteUserTypeOrmRepository implements IWriteUserRepository
             model.email = input.email;
             model.password = await hash(input.password, 10);
             
-            const roles = await queryRunner.manager.findByIds(RoleModel, input.roleIds);
-            const permissions = await queryRunner.manager.findByIds(PermissionModel, input.permissionIds);
-            roles.forEach(role => {
-                role.permissions = permissions;
-            });
-            const saveRoles = await queryRunner.manager.save<RoleModel>(roles);
+            const roles = await queryRunner.manager.find(RoleModel, { where: { id: In(input.roleIds) } });
+            model.roles = roles;
 
-            model.roles = saveRoles;
             res = await queryRunner.manager.save<UserModel>(model);
            
             await queryRunner.commitTransaction();
