a7d3333d29320094b3b12f5dc46ed6c8c623d1ee
paovang <vangpaovang@gmail.com>
2024-02-05 15:50:59 +0700

    update

--- src/modules/user/controllers/auth.controller.ts
+++ src/modules/user/controllers/auth.controller.ts
@@ -2,6 +2,9 @@ import { Public } from "@/common/decorators/public.decorator";
 import { Body, Controller, Get, Post } from "@nestjs/common";
 import { CommandBus } from "@nestjs/cqrs";
 import { CreateTestCommand } from "../commands/command/test/create-command";
+import { Permissions } from "@/common/decorators/permission.decorator";
+import { Permission } from "../domain/entities/permission.entity";
+import { Roles } from "@/common/decorators/role.decorator";
 
 @Controller('admin/auth')
 export class AuthController {
@@ -9,10 +12,12 @@ export class AuthController {
         private readonly _commandBus: CommandBus
     )
     {
-
+        
     }
 
-    @Public()
+    // @Public()
+    // @Permissions(Permission.CreateUsers)
+    @Roles('super-admin', 'admin')
     @Post('all')
     async getAll(
         @Body() body: any
